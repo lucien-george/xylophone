8a86394b4aa0b1d77ea7e38937176fa73c44dd37
Lucien George <lucien.george@shopify.com>
2022-11-22 09:19:04 -0500

    finished xylophone app

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,33 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        guard let currentTitle = sender.currentTitle else { return }
+        sender.layer.opacity = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.layer.opacity = 1
+        }
+        playSound(for: currentTitle)
+    }
     
+    func playSound(for key: String) {
+        guard let url = Bundle.main.url(forResource: key, withExtension: "wav") else { return }
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 
 }
 
